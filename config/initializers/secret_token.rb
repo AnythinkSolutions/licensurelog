e9edbd58a553517d6db028f03318dca2865ec00b
Brian Schwalm <bschwalm@anythinksolutions.com>
2013-11-29 12:26:36 -0700

    generated new rails app CertTracker

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CertTracker::Application.config.secret_key_base = '5e97026c8cf3ef1eb86c7d62afa931abfe5e6751f12763b8d4b2e21315e893a995a7de734f08cc059c410bc737c8330701e686e1358522e786e90916daea00a1'
