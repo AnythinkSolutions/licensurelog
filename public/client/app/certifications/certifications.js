0039a49599ac8355a98b97622a726a64bb4cd14c
Brian Schwalm <bschwalm@anythinksolutions.com>
2014-01-09 15:23:08 -0700

    working on a directive to create a chart.

--- public/client/app/certifications/certifications.js
+++ public/client/app/certifications/certifications.js
@@ -17,6 +17,19 @@ shell.controller('certificationController', ['$scope', '$http', '$routeParams', 
         var hours = [];
         $scope.certId = id;
 
+        $scope.chartData = [
+            { y: '2004', Item1: 2647, Item2: 0 ,Item3:2666 },
+            { y: '2005', Item1: 2778, Item2: 2294 ,Item3:2441 },
+            { y: '2006', Item1: 4912, Item2: 1969 ,Item3:2501 },
+            { y: '2007', Item1: 5689, Item2: 3597 ,Item3:3767 },
+            { y: '2008', Item1: 2293, Item2: 1914 ,Item3:6810 },
+            { y: '2009', Item1: 1881, Item2: 4293 ,Item3:5670 },
+            { y: '2010', Item1: 1588, Item2: 3795 ,Item3:4820 },
+            { y: '2011', Item1: 5174, Item2: 5967 ,Item3:15073 },
+            { y: '2012', Item1: 2028, Item2: 4460 ,Item3:10687 },
+            { y: '2013', Item1: 1791, Item2: 5713 ,Item3:8432 },
+        ];
+
         var prepareCategory = function(cat){
 
             //Checks to see if a
--- public/client/app/common/directives.js
+++ public/client/app/common/directives.js
@@ -28,29 +28,18 @@ shell.directive('a', function() {
 shell.directive('ctAreaChart', function(){
 
     return{
-        restrict: 'E',
+        restrict: 'A',
 //        scope: {
 //          data: "=ctData"
 //        },
         link: function(scope, element, attributes){
 
-            var data = scope.data;
+            var data = scope.chartData;
 
             //Morris Chart
             var overviewChart = Morris.Area({
-                element: 'overviewChart',
-                data: [
-                    { y: '2004', Item1: 2647, Item2: 0 ,Item3:2666 },
-                    { y: '2005', Item1: 2778, Item2: 2294 ,Item3:2441 },
-                    { y: '2006', Item1: 4912, Item2: 1969 ,Item3:2501 },
-                    { y: '2007', Item1: 5689, Item2: 3597 ,Item3:3767 },
-                    { y: '2008', Item1: 2293, Item2: 1914 ,Item3:6810 },
-                    { y: '2009', Item1: 1881, Item2: 4293 ,Item3:5670 },
-                    { y: '2010', Item1: 1588, Item2: 3795 ,Item3:4820 },
-                    { y: '2011', Item1: 5174, Item2: 5967 ,Item3:15073 },
-                    { y: '2012', Item1: 2028, Item2: 4460 ,Item3:10687 },
-                    { y: '2013', Item1: 1791, Item2: 5713 ,Item3:8432 },
-                ],
+                element: element[0].id,
+                data: data,
                 xkey: 'y',
                 ykeys: ['Item1', 'Item2' , 'Item3'],
                 labels: ['Item1', 'Item2', 'Item3'],
