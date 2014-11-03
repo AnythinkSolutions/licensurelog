92d7e736117e47348461ad092ff78185d3c036ee
Brian Schwalm <bschwalm@anythinksolutions.com>
2014-11-02 21:52:25 -0700

    notes for future work.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,9 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+
+        #todo: create the licensure template
+
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render action: 'show', status: :created, location: @user }
       else
--- app/models/certification.rb
+++ app/models/certification.rb
@@ -25,4 +25,6 @@ class Certification < ActiveRecord::Base
 
   end
 
+
+
 end
